442674a64c9f6ea843af80ae1fafde0781faa83b
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-21 20:08:51 +0600

    - Ep-02 created

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup>
+import {ref} from 'vue'
+const location  = ref('Dhaka');
+
+const setLocation = function(newLocation){
+    location.value = newLocation
+}
+
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
         <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
         <div class="container mx-auto flex space-x-5 justify-center m-5">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setLocation('Dhaka')">
                 Dhaka
             </button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setLocation('Rajshahi')">
                 Rajshahi
             </button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setLocation('Khulna')">
                 Khulna
             </button>
         </div>
-        <h1 class="text-2xl m-10">Current Location is </h1>
+        <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
     </section>
 </template>
 
