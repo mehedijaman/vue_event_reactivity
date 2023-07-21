301c95bd6200af88768baaa18f1939d6a9e699c0
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-21 20:02:32 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
+        <div class="container mx-auto flex space-x-5 justify-center m-5">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Dhaka
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Rajshahi
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Khulna
+            </button>
+        </div>
+        <h1 class="text-2xl m-10">Current Location is </h1>
+    </section>
+</template>
+
+<style scoped></style>
